686b742c048078a866499ac8f378bd2f7bd9c14a
k8w <me@k8w.io>
2021-05-28 20:38:49 +0800

    ws template

--- templates/client-browser/src-ws/index.ts
+++ templates/client-browser/src-ws/index.ts
@@ -2,6 +2,7 @@ import 'k8w-extend-native';
 import { WsClient } from 'tsrpc-browser';
 import { serviceProto } from './shared/protocols/serviceProto';
 
+// Create the WebSocket Client
 const client = new WsClient(serviceProto, {
     server: 'ws://127.0.0.1:3000',
     onStatusChange: v => {
@@ -13,33 +14,7 @@ const client = new WsClient(serviceProto, {
     }
 });
 
-function output(to: 'say' | 'public', content: string, color?: string) {
-    const container = document.getElementById(to === 'say' ? 'say-output' : 'public-msg') as HTMLDivElement;
-    container.innerHTML += `<p${color ? ` style="color:${color}"` : ''}>` + content + '</p>';
-    container.scrollTo(0, container.scrollHeight);
-}
-
-document.getElementById('btn-say')!.onclick = async () => {
-    const inputName = document.getElementById('input-name') as HTMLInputElement;
-
-    output('say', `Say: "${inputName.value}"`);
-
-    let ret = await client.callApi('Hello', {
-        name: inputName.value
-    });
-
-    if (!ret.isSucc) {
-        output('say', `❌ ${ret.err.message}`, 'red');
-        return;
-    }
-
-    output('say', `✅ Reply: ${ret.res.reply}`, 'green');
-};
-
-client.listenMsg('Public', msg => {
-    output('public', `<span style="color: gray;">${msg.time.format()}</span><br />${msg.content}`)
-});
-
+// Connect to the server
 async function connect() {
     output('say', `Connecting to the server...`);
     let resConnect = await client.connect();
@@ -51,3 +26,32 @@ async function connect() {
     }
 }
 connect();
+
+// Call API
+document.getElementById('btn-say')!.onclick = async () => {
+    const inputName = document.getElementById('input-name') as HTMLInputElement;
+    output('say', `Say: "${inputName.value}"`);
+
+    let ret = await client.callApi('Hello', {
+        name: inputName.value
+    });
+
+    // Handle Error
+    if (!ret.isSucc) {
+        output('say', `❌ ${ret.err.message}`, 'red');
+        return;
+    }
+
+    output('say', `✅ Reply: ${ret.res.reply}`, 'green');
+};
+
+// Listen server pushed message
+client.listenMsg('Public', msg => {
+    output('public', `<span style="color: gray;">${msg.time.format()}</span><br />${msg.content}`)
+});
+
+function output(to: 'say' | 'public', content: string, color?: string) {
+    const container = document.getElementById(to === 'say' ? 'say-output' : 'public-msg') as HTMLDivElement;
+    container.innerHTML += `<p${color ? ` style="color:${color}"` : ''}>` + content + '</p>';
+    container.scrollTo(0, container.scrollHeight);
+}
