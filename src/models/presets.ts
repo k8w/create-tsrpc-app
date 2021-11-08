2076f6a705f69c7bd8bea4ff212192a7c93b574f
k8w <me@k8w.io>
2021-11-08 21:57:53 +0800

    1.2.0-dev.1 ws presets

--- src/models/presets.ts
+++ src/models/presets.ts
@@ -4,26 +4,51 @@ export const presets: { [key: string]: Omit<CreateOptions, 'projectDir'> } = {
     server: {
         server: 'http',
         client: 'none',
-        features: ['unitTest']
+        features: []
     },
     browser: {
         server: 'http',
         client: 'browser',
-        features: ['symlink', 'unitTest']
+        features: []
     },
     react: {
         server: 'http',
         client: 'react',
-        features: ['symlink', 'unitTest']
+        features: []
     },
     vue2: {
         server: 'http',
         client: 'vue2',
-        features: ['symlink', 'unitTest']
+        features: []
     },
     vue3: {
         server: 'http',
         client: 'vue3',
-        features: ['symlink', 'unitTest']
+        features: []
+    },
+    'server-ws': {
+        server: 'ws',
+        client: 'none',
+        features: []
+    },
+    'browser-ws': {
+        server: 'ws',
+        client: 'browser',
+        features: []
+    },
+    'react-ws': {
+        server: 'ws',
+        client: 'react',
+        features: []
+    },
+    'vue2-ws': {
+        server: 'ws',
+        client: 'vue2',
+        features: []
+    },
+    'vue3-ws': {
+        server: 'ws',
+        client: 'vue3',
+        features: []
     },
 }
